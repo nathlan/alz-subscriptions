6ddfe1b2383cbc9796b4b11f914223503586b206
nathanjnorris <13533617+nathanjnorris@users.noreply.github.com>
2026-03-06 08:32:51 +0000

    fix: remove backend config

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "stterraformstate"
-    container_name       = "alz-subscriptions"
-    key                  = "landing-zones/main.tfstate"
-    use_oidc             = true
-  }
-}
